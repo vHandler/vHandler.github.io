72da75f94e212fe39fc7b4b6a74de480088edeb1
vHandler <vickiehandler@gmail.com>
2021-04-20 18:22:37 -0600

    start over

--- MyWebsite/server.js
+++ MyWebsite/server.js
@@ -1,6 +1,6 @@
 'use strict';
-var http = require('https');
-var port = 'vhandler.github.io';
+var http = require('http');
+var port = 1335;
 
 http.createServer(function (req, res) {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
